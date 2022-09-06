a0b72a48fb845c4ce360a74a35fb6d2efff83b70
sebastiansossah <86540563+sebastiansossah@users.noreply.github.com>
2022-09-06 17:39:12 -0500

    Update settings.py

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -141,8 +141,8 @@ EMAIL_BACKEND= "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST= "smtp.gmail.com"
 EMAIL_USE_TLS= True
 EMAIL_PORT= 587
-EMAIL_HOST_USER= "juansossa1468@gmail.com"
-EMAIL_HOST_PASSWORD= "uglzmcwtzpmyxmhy"
+EMAIL_HOST_USER= "@gmail.com"
+EMAIL_HOST_PASSWORD= ""
 
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
